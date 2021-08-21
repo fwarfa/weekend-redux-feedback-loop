b149c72f4a154ddcd41a72522ef3d17a1fb0f48e
Farah Warfa <fwarfa@gmail.com>
2021-08-21 18:57:24 -0500

    created and setup file for feeling component

---
+++ src/components/App/Feeling.jsx
@@ -0,0 +1,12 @@
+import {useSelector, useDispatch} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function Feeling () {
+    return (
+        <>
+            <h1>How are you feeling today?</h1>
+        </>
+    );
+}
+
+export default Feeling;
