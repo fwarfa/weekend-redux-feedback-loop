4aad056d66a8459b96251a4147957c5b9ce4cef8
Farah Warfa <fwarfa@gmail.com>
2021-08-21 18:48:58 -0500

    setup index.js file

--- src/index.js
+++ src/index.js
@@ -4,5 +4,40 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Reducers
+const feedbackReducer = (state = [], action) => {
+    // Feedback added 
+    switch (action.type) {
+        case 'SET_FEEDBACK':
+            return action.payload
+        default:
+    }
+
+    return state;
+};
+
+// Store
+const storeInstance = createStore(
+    combineReducers({
+        // Add Reducers as needed
+        feedbackReducer
+    }),   
+    applyMiddleware(
+        logger
+    ) 
+);
+
+// Wrap our App in a Provider, this makes Redux available in
+// our entire application
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
